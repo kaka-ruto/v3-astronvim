c1c3193b212664eb45d74e0f83aa4c692eb05c60
Kaka Ruto <borecollins@gmail.com>
2023-03-28 23:16:08 +0100

    Add wakatime community plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -19,4 +19,8 @@ return {
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   -- Split and join blocks of code with space m
   { import = "astrocommunity.editing-support.treej" },
+  -- Preview markdown with :Glow
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  -- Track programming activity
+  { import = "astrocommunity.media.vim-wakatime" },
 }
