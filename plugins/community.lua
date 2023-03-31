8f8200882d245f50cdc2d3143a50426fa5393e06
Kaka Ruto <borecollins@gmail.com>
2023-03-31 13:03:30 +0100

    Remove unused community plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,6 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- Copilot
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   -- Add mode to the statusline
