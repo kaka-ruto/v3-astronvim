12867af86724ea1ab8a3fe72342b5a5cfd4b9054
Kaka Ruto <borecollins@gmail.com>
2023-07-06 11:47:57 +0100

    Run tests using neotest

--- plugins/user.lua
+++ plugins/user.lua
@@ -63,4 +63,28 @@ return {
     end,
     event = "User AstroFile",
   },
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = { "olimorris/neotest-rspec", "zidhuss/neotest-minitest" },
+    ft = { "ruby" },
+    config = function()
+      -- get neotest namespace (api call creates or returns namespace)
+      local neotest_ns = vim.api.nvim_create_namespace "neotest"
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+      require("neotest").setup {
+        adapters = {
+          require "neotest-rspec",
+          require "neotest-minitest",
+        },
+      }
+    end,
+  },
 }
