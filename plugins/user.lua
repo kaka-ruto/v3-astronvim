65e9e905b16616ab73c98b8faeac2cd5e974b25c
Kaka Ruto <borecollins@gmail.com>
2023-08-22 10:51:12 +0100

    Remove neotest

--- plugins/user.lua
+++ plugins/user.lua
@@ -65,28 +65,4 @@ return {
     cmd = { "Other", "OtherSplit", "OtherVSplit" },
     keys = { { "<leader>a", "<CMD>OtherVSplit<CR>", desc = "Open alternate file in vertical split" } },
   },
-
-  {
-    "nvim-neotest/neotest",
-    dependencies = { "olimorris/neotest-rspec", "zidhuss/neotest-minitest" },
-    ft = { "ruby" },
-    config = function()
-      -- get neotest namespace (api call creates or returns namespace)
-      local neotest_ns = vim.api.nvim_create_namespace "neotest"
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
-      require("neotest").setup {
-        adapters = {
-          require "neotest-rspec",
-          require "neotest-minitest",
-        },
-      }
-    end,
-  },
 }
